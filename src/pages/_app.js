c526e06a3e6ae2d3288df89eadab41df0a2b7be9
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-24 20:00:04 -0400

    add google shopping api and bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import '@/components/styles/globals.css'
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
